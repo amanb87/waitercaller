34b5e938675026e80f7403f7e51e8d381c0ff2d3
Aman Buttan <amanbutt@in.ibm.com>
2018-05-02 01:23:30 +0530

    added logout functionality

--- waitercaller.py
+++ waitercaller.py
@@ -1,7 +1,7 @@
-
+import hashlib  #ships by default with Python 2.6+
 from flask import Flask,render_template, redirect, url_for,request
 from flask_login import LoginManager
-from flask_login import login_required, login_user
+from flask_login import login_required, login_user, logout_user
 from mockdbhelper import MockDBHelper as DBHelper
 from user import User
 
@@ -23,14 +23,26 @@ def login():
     user_password = DB.get_user(email)
     if user_password and user_password == password:
         user = User(email)
-        login_user(user)
+        login_user(user, remember=True)                #add remember argument for persistent login session
         return redirect(url_for('account'))
     return home()
+
+@login_manager.user_loader
+def load_user(user_id):
+    user_password = DB.get_user(user_id)
+    if user_password:
+        return User(user_id)
 
 @app.route("/account")
 @login_required
 def account():
     return "You are logged in"
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
