f4b78854764cd9f894647dd6caa70a5c97a739fc
Aman <abuttan87@gmail.com>
2018-07-02 16:10:25 +0530

    Update waitercaller.py

--- waitercaller.py
+++ waitercaller.py
@@ -17,7 +17,7 @@ PH = PasswwordHelper()
 BH = BitlyHelper()
 
 app = Flask(__name__)
-app.secret_key = 'PLM7rVfkgQDDSYo1dYZ1Ig=='
+app.secret_key = 'xxxxxxxxxx'
 login_manager = LoginManager(app)
 
 @app.route("/")
